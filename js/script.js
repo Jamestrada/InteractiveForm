ead44bbda282912c0c37a5f43eb7ab12c14fca9a
James Estrada <Jamestrada@users.noreply.github.com>
2020-05-05 23:35:52 -0600

    added other job role option functionality

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ Developed by: James Estrada
 Interactive web form.
 ***********************************************/
 
+const firstFocus = document.getElementById('name');
+firstFocus.focus();
+
+const jobRole = document.getElementById('title');
+//select and hide initially the other-job-role text field.
+const otherJobRole = document.getElementById('other-job-role'); 
+otherJobRole.style.display = 'none';
+
+// enable the 'other-job-role' text field only if the option the user selected was 'other'.
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value === 'other') {
+        otherJobRole.style.display = '';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
