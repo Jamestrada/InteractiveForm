f1bb12735fb99c3550d40fc72fd81c7abcf79e9a
James Estrada <Jamestrada@users.noreply.github.com>
2020-05-06 23:57:53 -0600

    Color options show until a theme is selected

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ jobRole.addEventListener('change', (e) => {
         otherJobRole.style.display = 'none';
     }
 });
+
+const selectTShirtTheme = document.createElement('option');
+selectTShirtTheme.value = 'select theme';
+selectTShirtTheme.textContent = 'Please select a T-shirt theme';
+const colorMenu = document.querySelector('#color');
+const firstColorOption = document.querySelector('option[value="cornflowerblue"]');
+colorMenu.insertBefore(selectTShirtTheme, firstColorOption);
+const tShirtColors = document.querySelectorAll('#color option');
+tShirtColors[0].selected = true;
+
+const hideColors = () => {
+    for (let i = 1; i < tShirtColors.length; i++) {
+        tShirtColors[i].hidden = true;
+    }
+};
+hideColors();
+
+const showColors = () => {
+    for (let i = 1; i < tShirtColors.length; i++) {
+        tShirtColors[i].hidden = false;
+    }
+};
+
+const tShirtDesign = document.querySelector('#design');
+tShirtDesign.addEventListener('change', (e) => {
+    if (e.target.value !== 'Select Theme') {
+        showColors();
+    } else {
+        hideColors();
+    }
+});
+
+
