4e8163e7215304315bce31e2d850755348f210e8
James Estrada <Jamestrada@users.noreply.github.com>
2020-05-09 23:37:27 -0600

    Display total cost & payment info based on choice

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ Interactive web form.
 const firstFocus = document.getElementById('name');
 firstFocus.focus();
 
+// Job Role section
+
 const jobRole = document.getElementById('title');
-//select and hide initially the other-job-role text field.
+// Select and hide initially the other-job-role text field.
 const otherJobRole = document.getElementById('other-job-role'); 
 otherJobRole.style.display = 'none';
 
-// enable the 'other-job-role' text field only if the option the user selected was 'other'.
+// Enable the 'other-job-role' text field only if the option the user selected was 'other'.
 jobRole.addEventListener('change', (e) => {
     if (e.target.value === 'other') {
         otherJobRole.style.display = '';
@@ -21,6 +23,8 @@ jobRole.addEventListener('change', (e) => {
         otherJobRole.style.display = 'none';
     }
 });
+
+// T-Shirt info section
 
 const selectTShirtTheme = document.createElement('option');
 selectTShirtTheme.value = 'select theme';
@@ -58,8 +62,14 @@ tShirtDesign.addEventListener('change', (e) => {
     }
 });
 
+// Register for Activities section
+
 const activities = document.querySelector('.activities');
 const checkboxes = document.querySelectorAll('.activities input');
+const total = document.createElement('h3');
+let cost = 0;
+activities.appendChild(total);
+total.textContent = `Total price: $${cost}`;
 
 activities.addEventListener('change', (e) => {
     const clicked = e.target;
@@ -71,5 +81,39 @@ activities.addEventListener('change', (e) => {
                 checkbox.disabled = false;
             }
         }
+    }
+    if (clicked.checked) {
+        cost += parseInt(clicked.dataset['cost']);
+    } else {
+        cost -= parseInt(clicked.dataset['cost']);
+    }
+    total.textContent = `Total price: $${cost}`;
+});
+
+// Payment info section
+const payment = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+const paymentMethods = payment.querySelectorAll('option');
+paymentMethods[0].disabled = true;
+paymentMethods[1].selected = true;
+// Since credit card is selected by default, hide the other payment options.
+paypal.hidden = true; 
+bitcoin.hidden = true; 
+
+payment.addEventListener('change', (e) => {
+    if (e.target.value === 'credit card') {
+        paypal.hidden = true;
+        bitcoin.hidden = true;
+        creditCard.hidden = false;
+    } else if (e.target.value === 'paypal') {
+        creditCard.hidden = true;
+        bitcoin.hidden = true;
+        paypal.hidden = false;
+    } else if (e.target.value === 'bitcoin') {
+        creditCard.hidden = true;
+        paypal.hidden = true;
+        bitcoin.hidden = false;
     }
 });
